c5bddbcd59b00867f21e7796e68e6ab3a1092625
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-26 15:48:11 +0600

    Создан компонент EditQuote, предоставляющий форму для редактирования существующей цитаты. Использование хуков useState для управления состоянием формы author, text, category. Реализован функционал получения данных цитаты из Firebase и заполнение формы. Обработчик отправки формы, отправляющий PUT-запрос к firebase API для обновления цитаты. После успешного обновления форма перенаправляет на главную страницу. Отображение формы с полями для автора, категории и текста цитаты.

---
+++ src/components/EditQuote/EditQuote.tsx
@@ -0,0 +1,79 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axiosAPI from '../../axiosAPI.ts';
+import * as React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { categories } from '../../categories.ts';
+
+const EditQuote = () => {
+  const {id} = useParams();
+  const [author, setAuthor] = useState('');
+  const [text, setText] = useState('');
+  const [category, setCategory] = useState('');
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const response = await axiosAPI.get(`/quotes/${id}.json`);
+        const quoteData = response.data;
+        setAuthor(quoteData.author);
+        setText(quoteData.text);
+        setCategory(quoteData.category);
+      } catch (error) {
+        console.error('error fetching quote:', error);
+      }
+    };
+    (async () => {
+      await fetchQuote();
+    })();
+  }, [id]);
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      await axiosAPI.put(`/quotes/${id}.json`, {author, text, category});
+      navigate('/');
+    } catch (error) {
+      console.error('error updating quote:', error);
+    }
+  }
+  return (
+    <Container>
+      <h1 className="my-4">Refactor quote</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formAuthor">
+          <Form.Label>Author</Form.Label>
+          <Form.Control
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            required/>
+        </Form.Group>
+        <Form.Group controlId="formCategory" className="mt-3">
+          <Form.Label>category</Form.Label>
+          <Form.Control
+            as="select"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            required>
+            <option value="">Select Category</option>
+            {categories.map(category => (
+              <option key={category.id} value={category.id}>{category.title}</option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="formText" className='mt-3'>
+          <Form.Label>Text</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            required/>
+        </Form.Group>
+        <Button variant='primary' type='submit' className='mt-3'>Save</Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default EditQuote;
