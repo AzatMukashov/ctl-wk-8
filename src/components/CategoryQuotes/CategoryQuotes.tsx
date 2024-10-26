d8a11fc78cea1ae7c2a3951dc9c29a22774365ad
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-26 14:50:25 +0600

    Создан компонент CategoryQuotes, отвечающий за отображение цитат по выбранной категории. Использование useParams для извлечения категории из URL. Вертикальное меню с категориями отображается в левом столбце. Добавлена категория "All" для отображения всех цитат. Компонент QuoteList отображает цитаты для выбранной категории в правом столбце.

---
+++ src/components/CategoryQuotes/CategoryQuotes.tsx
@@ -0,0 +1,30 @@
+import { Link, useParams } from 'react-router-dom';
+import QuoteList from '../QuoteList/QuoteList.tsx';
+import { categories } from '../../categories.ts';
+import { Col, Container, Nav, Row } from 'react-bootstrap';
+
+const CategoryQuotes = () => {
+  const {category} = useParams<{ category: string }>();
+  return (
+    <Container>
+      <Row>
+        <Col md={3} className="mb-4">
+          <Nav className="flex-column">
+            {categories.map(cat => (
+              <Nav.Item key={cat.id}>
+                <Nav.Link as={Link} to={cat.id === 'all' ? '/' : `/category/${cat.id}`}>
+                  {cat.title}</Nav.Link>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </Col>
+        <Col md={9}>
+          <h1>Quotes in category: {category}</h1>
+          <QuoteList category={category === 'all' ? '' : category}/>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default CategoryQuotes;
