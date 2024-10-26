d6247d6313497dd32e45b33f492256e48be12ef4
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-26 11:47:23 +0600

    создал массив категорий

---
+++ src/categories.ts
@@ -0,0 +1,7 @@
+export const categories = [
+  {title: 'Star Wars', id: 'star-wars'},
+  {title: 'Motivational', id: 'motivational'},
+  {title: 'Famous People', id: 'famous-people'},
+  {title: 'Humour', id: 'humour'},
+  {title: 'Saying', id: 'saying'},
+]
