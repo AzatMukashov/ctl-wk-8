d55afe018177f23c664be57cb976de9fe1c7464e
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-26 15:29:12 +0600

    Создан компонент AddQuote, предоставляющий форму для добавления новой цитаты. Использование хуков useState для управления состоянием формы author, text, category. Реализован обработчик отправки формы, отправляющий POST-запрос к firebase API для добавления цитаты. После успешного добавления цитаты форма очищается. Отображение формы с полями для автора, категории и текста цитаты.

---
+++ src/components/AddQuote/AddQuote.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import * as React from 'react';
+import axiosAPI from '../../axiosAPI.ts';
+import { Button, Container, Form } from 'react-bootstrap';
+import { categories } from '../../categories.ts';
+
+const AddQuote = () => {
+  const [author, setAuthor] = useState('');
+  const [text, setText] = useState('');
+  const [category, setCategory] = useState('');
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      await axiosAPI.post('/quotes.json', {author, text, category});
+      setAuthor('');
+      setText('');
+      setCategory('');
+    } catch (error) {
+      console.error('error adding quote:', error);
+    }
+  }
+  return (
+    <Container>
+      <h1 className="my-4">Add new quote</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formAuthor">
+          <Form.Label>Author</Form.Label>
+          <Form.Control
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formCategory" className="mt-3">
+          <Form.Label>Category</Form.Label>
+          <Form.Control
+            as="select"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            required
+          >
+            <option value="">Select Category</option>
+            {categories.map(category => (
+              <option key={category.id} value={category.id}>{category.title}</option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="formText" className="mt-3">
+          <Form.Label>Text</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Button variant='primary' type='submit' className='mt-3'>Add</Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default AddQuote;
