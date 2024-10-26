19b81e1b91412e4934b12364f7893f2b1a101edd
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-26 16:20:06 +0600

    Перемещены объекты в идентичном порядке с примером из ТЗ

--- src/categories.ts
+++ src/categories.ts
@@ -1,8 +1,8 @@
 export const categories = [
   {title: 'All', id: 'all'},
   {title: 'Star Wars', id: 'star-wars'},
-  {title: 'Motivational', id: 'motivational'},
   {title: 'Famous People', id: 'famous-people'},
-  {title: 'Humour', id: 'humour'},
   {title: 'Saying', id: 'saying'},
+  {title: 'Humour', id: 'humour'},
+  {title: 'Motivational', id: 'motivational'},
 ]
