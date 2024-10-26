d4a197e7f155320e77fa8010069014f40d199e77
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-26 18:36:51 +0600

    добавил иконки для редактирования и удаления как в примере ТЗ добавил стили для них

--- src/components/QuoteList/QuoteList.tsx
+++ src/components/QuoteList/QuoteList.tsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react';
 import axiosAPI from '../../axiosAPI.ts';
 import { Button, Card, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import type {Quote, QuoteList} from '../../types';
+import type { Quote, QuoteList } from '../../types';
+import refactorIcon from '../../assets/refactor.png';
+import deleteIcon from '../../assets/delete.png';
 
 const QuoteList: React.FC<QuoteList> = ({category}) => {
   const [quotes, setQuotes] = useState<Quote[]>([]);
   useEffect(() => {
     const fetchQuotes = async () => {
       try {
-        const response: {data: Record<string, Omit<Quote, 'id'>> | null} = await axiosAPI.get(
+        const response: { data: Record<string, Omit<Quote, 'id'>> | null } = await axiosAPI.get(
           category ? `/quotes.json?orderBy="category"&equalTo="${category}"` : '/quotes.json'
         );
         const quoteData = response.data;
@@ -23,7 +25,7 @@ const QuoteList: React.FC<QuoteList> = ({category}) => {
       await fetchQuotes();
     })();
   }, [category]);
-  const handleDelete = async (id:string) => {
+  const handleDelete = async (id: string) => {
     try {
       await axiosAPI.delete(`/quotes/${id}.json`);
       setQuotes(quotes.filter(quote => quote.id !== id));
@@ -34,20 +36,33 @@ const QuoteList: React.FC<QuoteList> = ({category}) => {
   return (
     <Container>
       {quotes.length > 0 ? quotes.map(quote => (
-        <Card className='mb-4' key={quote.id}>
+        <Card className="mb-4" key={quote.id} style={{position: 'relative'}}>
           <Card.Body>
-            <Card.Title>{quote.author}</Card.Title>
-            <Card.Text>{quote.text}</Card.Text>
-            <Card.Text>
-              <small className='text-muted'>Category: {quote.category}</small>
-            </Card.Text>
-            <Button variant='danger' onClick={() => handleDelete(quote.id)} className='me-2'>Delete</Button>
+            <Card.Text>"{quote.text}"</Card.Text>
+            <Card.Title>&mdash; {quote.author}</Card.Title>
+            <Button variant="danger" onClick={() => handleDelete(quote.id)} style={{
+              position: 'absolute',
+              top: '8px',
+              right: '20px',
+              background: 'none',
+              border: 'none'
+            }}>
+              <img src={deleteIcon} alt="Delete" width="29.5px" height="29.5px"/>
+            </Button>
             <Link to={`/edit-quote/${quote.id}`}>
-              <Button variant="primary">Refactor</Button>
+              <Button variant="primary" style={{
+                position: 'absolute',
+                top: '10px',
+                right: '60px',
+                background: 'none',
+                border: 'none'
+              }}>
+                <img src={refactorIcon} alt="Refactor" width="27.5px" height="27.5px"/>
+              </Button>
             </Link>
           </Card.Body>
         </Card>
-      )): <p>no quotes available</p>}
+      )) : <p>no quotes available</p>}
     </Container>
   );
 };
