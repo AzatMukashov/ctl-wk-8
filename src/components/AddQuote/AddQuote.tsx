2c0f1e3d10ae8cf13bda2d3fd953168652bf01a9
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-26 18:41:55 +0600

    переименовал название страницы, переместил элементы, добавил стили

--- src/components/AddQuote/AddQuote.tsx
+++ src/components/AddQuote/AddQuote.tsx
@@ -21,18 +21,9 @@ const AddQuote = () => {
   }
   return (
     <Container>
-      <h1 className="my-4">Add new quote</h1>
+      <h1 className="my-4">Submit new quote</h1>
       <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formAuthor">
-          <Form.Label>Author</Form.Label>
-          <Form.Control
-            type="text"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formCategory" className="mt-3">
+        <Form.Group controlId="formCategory" className='w-50'>
           <Form.Label>Category</Form.Label>
           <Form.Control
             as="select"
@@ -46,8 +37,18 @@ const AddQuote = () => {
             ))}
           </Form.Control>
         </Form.Group>
-        <Form.Group controlId="formText" className="mt-3">
-          <Form.Label>Text</Form.Label>
+        <Form.Group controlId="formAuthor" className="mt-3 w-75">
+          <Form.Label>Author</Form.Label>
+          <Form.Control
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formText" className="mt-3 w-75">
+          <Form.Label>Quote text</Form.Label>
           <Form.Control
             as="textarea"
             rows={3}
@@ -56,7 +57,7 @@ const AddQuote = () => {
             required
           />
         </Form.Group>
-        <Button variant='primary' type='submit' className='mt-3'>Add</Button>
+        <Button variant="primary" type="submit" className="mt-3 mb-4">Add</Button>
       </Form>
     </Container>
   );
